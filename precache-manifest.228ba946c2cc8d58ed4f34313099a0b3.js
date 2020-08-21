b7c1419377c0cc8493da7910bd67ce4a8820e412
Darina <kolodad00@gmail.com>
2020-08-21 21:55:55 +0300

    Updates

---
+++ precache-manifest.228ba946c2cc8d58ed4f34313099a0b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca14a348950d67f707a01a40919a3061",
+    "url": "/gallery/index.html"
+  },
+  {
+    "revision": "f0b655405ab4f54c8c6a",
+    "url": "/gallery/static/css/main.973aa9ca.chunk.css"
+  },
+  {
+    "revision": "6e8d9ef58539124d7bbc",
+    "url": "/gallery/static/js/2.7af169b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gallery/static/js/2.7af169b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0b655405ab4f54c8c6a",
+    "url": "/gallery/static/js/main.6cb2d495.chunk.js"
+  },
+  {
+    "revision": "60661d2bef486ca96e65",
+    "url": "/gallery/static/js/runtime-main.df3158ca.js"
+  }
+]);
